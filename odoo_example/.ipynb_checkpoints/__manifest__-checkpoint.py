953da1783eaf712cea7d464b9a28c2c06341ca71
Jose Henostroza <jose.henostroza@synet.com.pe>
2022-02-13 22:13:32 +0000

    [ADD] Basic Module

---
+++ odoo_example/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+{
+	'name': 'Odoo Academy',
+	
+	'summary': """Academia de Entrenamiento app""",
+
+	'description': """
+		Modulos a dictar por la Academia:
+		-cursos
+		-sesiones
+		-Alumnos
+	""",
+
+	'author': 'Odoo',
+
+	'website': 'http:synet.com.pe',
+
+	'category': 'Training',
+
+	'version': '0.1',
+
+	'depends': ['base'],
+
+	'data': [
+
+
+
+	],
+
+	'demo': [
+
+
+
+	],
+
+}
